6d7d8822105abfd97bf49513fbcc8800647b4728
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2020-09-13 17:09:21 +0300

    changed some tiles/added tooltip for play and stop btns

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,26 +1,28 @@
 import { addZero } from "./supportScript.js";
 
 export const videoPlayerInitialization = () => {
-   const videoPlayer = document.querySelector(".video-container__player")
-   const videoBtnPlay = document.querySelector(".btn-play")
-   const videoBtnStop = document.querySelector(".btn-stop")
-   const videoTimePassed = document.querySelector(".video-time-passed")
-   const videoProgressBar = document.querySelector(".video-container__video-progress")
-   const videoTimeTotal = document.querySelector(".video-time-total")
-   const videoVolume = document.querySelector(".video-container__video-volume")
-   const volumeUp = document.querySelector(".volume-up")
-   const volumeDown = document.querySelector(".volume-down")
-   const videoFullscreen = document.querySelector(".video-container__video-fullscreen")
+   const videoPlayer = document.querySelector(".video-container__player"),
+      videoBtnPlay = document.querySelector(".btn-play"),
+      videoBtnStop = document.querySelector(".btn-stop"),
+      videoTimePassed = document.querySelector(".video-time-passed"),
+      videoProgressBar = document.querySelector(".video-container__video-progress"),
+      videoTimeTotal = document.querySelector(".video-time-total"),
+      videoVolume = document.querySelector(".video-container__video-volume"),
+      volumeUp = document.querySelector(".volume-up"),
+      volumeDown = document.querySelector(".volume-down"),
+      videoFullscreen = document.querySelector(".video-container__video-fullscreen");
 
    // change icon(from play to pause) while clicking on player itself
    const changeIcon = () => {
       if (videoPlayer.paused) {
          videoBtnPlay.classList.remove("fa-pause");
          videoBtnPlay.classList.add("fa-play");
+         videoBtnPlay.setAttribute("data-title", "Play (k)"); //  set tooltip on hover for play and pause btn
 
       } else {
          videoBtnPlay.classList.add("fa-pause");
          videoBtnPlay.classList.remove("fa-play");
+         videoBtnPlay.setAttribute("data-title", "Pause (k)");
       }
    };
 
@@ -38,9 +40,38 @@ export const videoPlayerInitialization = () => {
    const stopVideoPlayer = () => {
       videoPlayer.pause();
       videoPlayer.currentTime = 0; // reset playing time to 0
+      videoPlayer.setAttribute("data-title", "Stop (k)");
 
       changeIcon();
    };
+
+   // updated vido time passed and video time total
+   const updateVideoTime = () => {
+      const currentVideoTime = videoPlayer.currentTime;
+      const videoDuration = videoPlayer.duration;
+
+      //round seconds and minutes for a current time of playing video
+      let minutesPassed = Math.floor(currentVideoTime / 60);
+      let secondsPassed = Math.floor(currentVideoTime % 60);
+
+      //round seconds and minutes for a total time of playing video
+      let minutesTotal = Math.floor(videoDuration / 60);
+      let secondsTotal = Math.floor(videoDuration % 60);
+
+      //change value of video progress bar (input)
+      videoProgressBar.value = (currentVideoTime / videoDuration) * 100;
+
+      videoTimePassed.textContent = `${addZero(minutesPassed)}:${addZero(secondsPassed)}`;
+      videoTimeTotal.textContent = `${addZero(minutesTotal)}:${addZero(secondsTotal)}`;
+   };
+
+   // rewind video by using video progress bar
+   const rewindVideo = () => {
+      const videoDuration = videoPlayer.duration;
+      const inputValue = videoProgressBar.value;
+
+      videoPlayer.currentTime = (inputValue * videoDuration) / 100;
+   }
 
    // increase video volume
    const increaseVolume = () => {
@@ -67,39 +98,14 @@ export const videoPlayerInitialization = () => {
    videoBtnPlay.addEventListener("click", runVideoPlayer);
    videoBtnStop.addEventListener("click", stopVideoPlayer);
 
-   videoPlayer.addEventListener("timeupdate", () => { // listen to timeupdate event for changes on video time passed and video total time 
-      const currentVideoTime = videoPlayer.currentTime;
-      const videoDuration = videoPlayer.duration;
-
-      //round seconds and minutes for a current time of playing video
-      let minutesPassed = Math.floor(currentVideoTime / 60);
-      let secondsPassed = Math.floor(currentVideoTime % 60);
-
-      //round seconds and minutes for a total time of playing video
-      let minutesTotal = Math.floor(videoDuration / 60);
-      let secondsTotal = Math.floor(videoDuration % 60);
-
-      //change value of video progress bar (input)
-      videoProgressBar.value = (currentVideoTime / videoDuration) * 100;
-
-      videoTimePassed.textContent = `${addZero(minutesPassed)}:${addZero(secondsPassed)}`;
-      videoTimeTotal.textContent = `${addZero(minutesTotal)}:${addZero(secondsTotal)}`;
-   });
-   
-   videoProgressBar.addEventListener("input", () => { // listen to input event on a video progress bar in order to rewind a video
-      const videoDuration = videoPlayer.duration;
-      const inputValue = videoProgressBar.value;
-
-      videoPlayer.currentTime = (inputValue * videoDuration) / 100;
-   });
+   videoPlayer.addEventListener("timeupdate", updateVideoTime) // listen to timeupdate event for changes on video time passed and video total time 
+   videoProgressBar.addEventListener("input", rewindVideo) // listen to input event on a video progress bar in order to rewind a video
 
    //videoPlayer.addEventListener("keydown", spacebarPressed);
 
    videoFullscreen.addEventListener("click", () => { // making video player fullscreen on click  to a btn
       videoPlayer.requestFullscreen();
    });
-
-
 
 
    // function calls
